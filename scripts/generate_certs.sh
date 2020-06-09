ca810f31e2fabbe9bb46688ce3e4620ce7b83866
Bryce Klinker <bryce.klinker@gmail.com>
2020-06-08 22:22:05 -0500

    Trying one more thing to get dev certs to work correctly

--- scripts/generate_certs.sh
+++ scripts/generate_certs.sh
@@ -6,8 +6,7 @@ PFX_CERT_PATH="${HOME}/.aspnet/https/haus.pfx"
 CERT_PASSWORD="haus"
 
 generate_dev_cert() {
-#    sudo security set-key-partition-list -D localhost -S unsigned:,teamid:UBF8T346G9
-    dotnet dev-certs https --clean
+    sudo security set-key-partition-list -D localhost -S unsigned:,teamid:UBF8T346G9 ~/Library/Keychains/Login.keychain
     dotnet dev-certs https -ep ${PFX_CERT_PATH} -p ${CERT_PASSWORD}
 }
 
