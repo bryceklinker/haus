939b970318203254e59fb7cd237c8cfae769229d
Bryce Klinker <bryce.klinker@gmail.com>
2020-06-08 22:55:14 -0500

    Attempting to use sudo for cert commands

--- scripts/generate_certs.sh
+++ scripts/generate_certs.sh
@@ -6,16 +6,16 @@ PFX_CERT_PATH="${HOME}/.aspnet/https/haus.pfx"
 CERT_PASSWORD="haus"
 
 generate_dev_cert() {
-    sudo security set-key-partition-list -D localhost -S unsigned:,teamid:UBF8T346G9 /Library/Keychains/System.keychain
-    dotnet dev-certs https -ep ${PFX_CERT_PATH} -p ${CERT_PASSWORD}
+#    sudo security set-key-partition-list -D localhost -S unsigned:,teamid:UBF8T346G9 /Library/Keychains/System.keychain
+    sudo dotnet dev-certs https -ep ${PFX_CERT_PATH} -p ${CERT_PASSWORD}
 }
 
 trust_dev_cert() {
-    dotnet dev-certs https --trust
+    sudo dotnet dev-certs https --trust
 }
 
 main() {
-#  generate_dev_cert
+  generate_dev_cert
   trust_dev_cert
 }
 
