9b62303de46a4c5094e4d30fa73ff6f5a6752faa
Bryce Klinker <bryce.klinker@gmail.com>
2020-06-08 20:58:20 -0500

    Updating script to provide password

--- scripts/generate_certs.sh
+++ scripts/generate_certs.sh
@@ -12,7 +12,7 @@ generate_dev_cert() {
 }
 
 trust_dev_cert() {
-    openssl pkcs12 -in ${PFX_CERT_PATH} -nokeys -out ${CRT_CERT_PATH} -nodes
+    openssl pkcs12 -in ${PFX_CERT_PATH} -nokeys -out ${CRT_CERT_PATH} -nodes -passin ${CERT_PASSWORD}
     cp ${CRT_CERT_PATH} ${CA_CERTS_PATH}
     sudo update-ca-certificates
     openssl verify ${CRT_CERT_PATH}
