d88894ef189c7688c62c0d2e807ad048bec9fdc9
Bryce Klinker <bryce.klinker@gmail.com>
2020-06-08 22:07:16 -0500

    Updating to allow certs accross partitions

--- scripts/generate_certs.sh
+++ scripts/generate_certs.sh
@@ -6,6 +6,7 @@ PFX_CERT_PATH="${HOME}/.aspnet/https/haus.pfx"
 CERT_PASSWORD="haus"
 
 generate_dev_cert() {
+    sudo security set-key-partition-list -D localhost -S unsigned:,teamid:UBF8T346G9
     dotnet dev-certs https -ep ${PFX_CERT_PATH} -p ${CERT_PASSWORD}
 }
 
