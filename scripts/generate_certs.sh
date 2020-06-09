5c23608b62f2397cf4df966493b073b3de0f809a
Bryce Klinker <bryce.klinker@gmail.com>
2020-06-08 23:42:11 -0500

    Trying to figure out if a cert already exists

--- scripts/generate_certs.sh
+++ scripts/generate_certs.sh
@@ -6,12 +6,14 @@ PFX_CERT_PATH="${HOME}/.aspnet/https/haus.pfx"
 CERT_PASSWORD="haus"
 
 generate_dev_cert() {
-    sudo security set-key-partition-list -D localhost -S unsigned:,teamid:UBF8T346G9 /Users/runner/Library/Keychains/login.keychain-db
-    sudo dotnet dev-certs https -ep ${PFX_CERT_PATH} -p ${CERT_PASSWORD}
+#    sudo security set-key-partition-list -D localhost -S unsigned:,teamid:UBF8T346G9 /Users/runner/Library/Keychains/login.keychain-db
+#    sudo dotnet dev-certs https -ep ${PFX_CERT_PATH} -p ${CERT_PASSWORD}
+    echo "WAIT"
 }
 
 trust_dev_cert() {
-    sudo dotnet dev-certs https --trust
+#    sudo dotnet dev-certs https --trust
+    ls -la ~/.aspnet/https
 }
 
 main() {
