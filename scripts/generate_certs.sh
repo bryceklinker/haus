dcc076fdbf386f7b9081023599f423c636076921
Bryce Klinker <bryce.klinker@gmail.com>
2020-06-08 21:48:25 -0500

    Using mac as I'm tired of fighting with linux

--- scripts/generate_certs.sh
+++ scripts/generate_certs.sh
@@ -2,40 +2,19 @@
 
 set -ex
 
-CERT_DIRECTORY="${HOME}/.aspnet/https"
-CERT_CONF_PATH="./scripts/haus-cert.conf"
-CERT_KEY_PATH="${CERT_DIRECTORY}/haus.key"
-CERT_PFX_PATH="${CERT_DIRECTORY}/haus.pfx"
-CERT_CRT_PATH="${CERT_DIRECTORY}/haus.crt"
+PFX_CERT_PATH="${HOME}/.aspnet/https/haus.pfx"
 CERT_PASSWORD="haus"
 
 generate_dev_cert() {
-    mkdir -p ${CERT_DIRECTORY}
-    openssl req -x509 \
-        -nodes \
-        -days 365 \
-        -newkey rsa:2048 \
-        -keyout ${CERT_KEY_PATH} \
-        -out ${CERT_CRT_PATH} \
-        -subj '/CN=localhost'
-}
-
-convert_cert_to_pfx() {
-    openssl pkcs12 -export \
-        -out ${CERT_PFX_PATH} \
-        -inkey ${CERT_KEY_PATH} \
-        -in ${CERT_CRT_PATH} \
-        -passout pass:${CERT_PASSWORD}
+    dotnet dev-certs https -ep ${PFX_CERT_PATH} -p ${CERT_PASSWORD}
 }
 
 trust_dev_cert() {
- sudo cp ${CERT_CRT_PATH} /usr/local/share/ca-certificates
- sudo update-ca-certificates
+    dotnet dev-certs https --trust
 }
 
 main() {
   generate_dev_cert
-  convert_cert_to_pfx
   trust_dev_cert
 }
 
