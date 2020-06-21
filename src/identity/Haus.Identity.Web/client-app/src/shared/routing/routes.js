4499a12008de5ecec8ffc8710269d5ee062248ee
Bryce Klinker <bryce.klinker@gmail.com>
2020-06-21 16:26:15 -0500

    Adding users to routing

--- src/identity/Haus.Identity.Web/client-app/src/shared/routing/routes.js
+++ src/identity/Haus.Identity.Web/client-app/src/shared/routing/routes.js
@@ -4,16 +4,19 @@ import React from "react";
 import {WelcomeView} from "../../welcome/views/WelcomeView";
 import {ProtectedRoute} from "./ProtectedRoute";
 import {AuthCallbackView} from "../authentication/AuthCallbackView";
+import {UserListContainer} from '../../users/containers/UsersListContainer';
 
 export const ROUTING_PATHS = {
     WELCOME: '/welcome',
-    AUTH_CALLBACK: '/auth-callback'
+    AUTH_CALLBACK: '/auth-callback',
+    USERS: '/users'
 };
 
 export function Routes() {
     return (
         <Switch>
             <ProtectedRoute path={ROUTING_PATHS.WELCOME} component={WelcomeView} />
+            <ProtectedRoute path={ROUTING_PATHS.USERS} component={UserListContainer} />
             <Route path={ROUTING_PATHS.AUTH_CALLBACK} component={AuthCallbackView}/>
             <Redirect to={ROUTING_PATHS.WELCOME} from={'/**'}/>
         </Switch>
