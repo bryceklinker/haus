3e94c570d0798bf8fdfbe031d80baf3fdfc71e48
Bryce Klinker <bryce.klinker@gmail.com>
2020-12-01 23:20:00 -0600

    Updating build process to generate code coverage report

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,19 +1,45 @@
 #!/usr/bin/env bash
 set -ex
 
+WORKING_DIRECTORY=$(pwd)
 CONFIGURATION=${CONFIGURATION:-'Release'}
-PUBLISH_DIRECTORY=${PUBLISH_DIRECTORY:-'./publish'}
+PUBLISH_DIRECTORY="${WORKING_DIRECTORY}/publish"
+COVERAGE_FILE_PATH="${WORKING_DIRECTORY}/coverage.json"
+COBERTURA_FILE_PATH="${WORKING_DIRECTORY}/coverage.cobertura.xml"
+IS_RELEASE=${IS_RELEASE:-false}
 
-FRONT_END_DIRECTORY="./src/Haus.Web.Host/client-app"
-WEB_HOST_PROJECT="./src/Haus.Web.Host/Haus.Web.Host.csproj"
-ZIGBEE_HOST_PROJECT="./src/Haus.Zigbee.Host/Haus.Zigbee.Host.csproj"
+FRONT_END_DIRECTORY="${WORKING_DIRECTORY}/src/Haus.Web.Host/client-app"
+WEB_HOST_PROJECT="${WORKING_DIRECTORY}/src/Haus.Web.Host/Haus.Web.Host.csproj"
+ZIGBEE_HOST_PROJECT="${WORKING_DIRECTORY}/src/Haus.Zigbee.Host/Haus.Zigbee.Host.csproj"
 
 function build_solution() {
-  dotnet build --configuration "${CONFIGURATION}"
+  dotnet build
+}
+
+function run_dotnet_test() {
+  PROJECT_NAME=$1
+  PROJECT_PATH="${WORKING_DIRECTORY}/tests/${PROJECT_NAME}"
+  dotnet coverlet "${PROJECT_PATH}/bin/Debug/net5.0/${PROJECT_NAME}.dll" \
+    --target "dotnet" \
+    --targetargs "test ${PROJECT_PATH} --no-build" \
+    --merge-with "${COVERAGE_FILE_PATH}" \
+    --format cobertura \
+    --format json
 }
 
 function run_tests() {
-  dotnet test
+  dotnet tool restore
+  
+  run_dotnet_test "Haus.Core.Tests"
+  run_dotnet_test "Haus.Utilities.Tests"
+  run_dotnet_test "Haus.Web.Host.Tests"
+  run_dotnet_test "Haus.Zigbee.Host.Tests"
+  
+  dotnet reportgenerator \
+    "-reports:${COBERTURA_FILE_PATH}" \
+    "-targetdir:coveragereport" \
+    "-reporttypes:Html"
+    
   pushd "${FRONT_END_DIRECTORY}" || exit
     yarn install
     yarn test
@@ -49,6 +75,10 @@ function dotnet_publish() {
 }
 
 function publish_app() {
+  if [ "${IS_RELEASE}" == "false" ]; then
+    exit 0
+  fi
+  
   dotnet_publish $WEB_HOST_PROJECT "${PUBLISH_DIRECTORY}/web_host"
   dotnet_publish $ZIGBEE_HOST_PROJECT "${PUBLISH_DIRECTORY}/zigbee_host"
 }
