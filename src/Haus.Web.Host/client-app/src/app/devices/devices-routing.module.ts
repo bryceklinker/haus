16b05e9dacf84182cc42ba4e8c672574bd2ffc48
Bryce Klinker <bryce.klinker@gmail.com>
2020-12-29 23:25:34 -0600

    Adding device detail route back to app

--- src/Haus.Web.Host/client-app/src/app/devices/devices-routing.module.ts
+++ src/Haus.Web.Host/client-app/src/app/devices/devices-routing.module.ts
@@ -1,13 +1,14 @@
 import {RouterModule, Routes} from "@angular/router";
 import {NgModule} from "@angular/core";
 import {DevicesRootComponent} from "./components/devices-root/devices-root.component";
+import {DeviceDetailRootComponent} from "./components/device-detail-root/device-detail-root.component";
 
 const routes: Routes = [
   {
     path: '',
     component: DevicesRootComponent,
     children: [
-      // {path: ':deviceId', component: DeviceDetailContainerComponent}
+      {path: ':deviceId', component: DeviceDetailRootComponent}
     ]
   }
 ];
