b067e2b6e48e66b3a9afe50285dba6e74b715383
Bryce Klinker <bryce.klinker@gmail.com>
2020-12-31 00:48:42 -0600

    Continuing to use record instead of class where applicable

--- src/Haus.Core.Models/Rooms/Events/RoomLightingChangedEvent.cs
+++ src/Haus.Core.Models/Rooms/Events/RoomLightingChangedEvent.cs
@@ -3,21 +3,13 @@ using Haus.Core.Models.ExternalMessages;
 
 namespace Haus.Core.Models.Rooms.Events
 {
-    public class RoomLightingChangedEvent : IHausCommandCreator<RoomLightingChangedEvent>
+    public record RoomLightingChangedEvent(RoomModel Room, LightingModel Lighting) : IHausCommandCreator<RoomLightingChangedEvent>
     {
         public const string Type = "room_lighting_changed";
-        public RoomModel Room { get; }
-        public LightingModel Lighting { get; }
-
-        public RoomLightingChangedEvent(RoomModel room, LightingModel lighting)
-        {
-            Room = room;
-            Lighting = lighting;
-        }
 
         public HausCommand<RoomLightingChangedEvent> AsHausCommand()
         {
-            return new HausCommand<RoomLightingChangedEvent>(Type, this);
+            return new(Type, this);
         }
     }
 }
