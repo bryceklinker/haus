e0505777d79b67bfa10acbdf9281b32015ac074f
Bryce Klinker <bryce.klinker@gmail.com>
2021-02-04 21:01:39 -0600

    fix: Fixing unit test

--- src/Haus.Web.Host/client-app/src/app/shared/components/loading-dialog/loading-dialog.component.ts
+++ src/Haus.Web.Host/client-app/src/app/shared/components/loading-dialog/loading-dialog.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject} from "@angular/core";
+import {Component, Inject, OnInit} from "@angular/core";
 import {Actions} from "@ngrx/effects";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {LoadingDialogOptions} from "./loading-dialog.options";
@@ -8,7 +8,7 @@ import {LoadingDialogOptions} from "./loading-dialog.options";
   templateUrl: './loading-dialog.component.html',
   styleUrls: ['./loading-dialog.component.scss']
 })
-export class LoadingDialogComponent {
+export class LoadingDialogComponent implements OnInit {
 
   get text(): string {
     return this.model.text;
@@ -17,5 +17,9 @@ export class LoadingDialogComponent {
   constructor(private readonly actions$: Actions,
               private readonly dialogRef: MatDialogRef<LoadingDialogComponent>,
               @Inject(MAT_DIALOG_DATA) private readonly model: LoadingDialogOptions) {
+  }
+
+  ngOnInit(): void {
+    this.dialogRef.disableClose = true;
   }
 }
