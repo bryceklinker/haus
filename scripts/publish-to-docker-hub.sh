ed43a9cbb3f0403f02ee31bc898ef3d7628b882f
Bryce Klinker <bryce.klinker@gmail.com>
2021-11-30 21:05:30 -0600

    Updating to publish docker images instead of locally running applications. This seems like a safer bet moving forward

---
+++ scripts/publish-to-docker-hub.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -ex
+
+source ./scripts/variables.sh
+
+function login() {
+  docker login -u "${DOCKER_HUB_USERNAME}" -p "${DOCKER_HUB_ACCESS_TOKEN}"
+}
+
+function build_docker_image() {
+  PUBLISH_DIRECTORY="${1}"
+  APPLICATION_NAME="${2}"
+  docker build . --tag "haus/${APPLICATION_NAME}:${VERSION}" --file "${APPLICATION_NAME}-dockerfile"
+}
+
+function main() {
+  login
+  build_docker_image "${WEB_HOST_PUBLISH_DIRECTORY}" "haus-web"
+  build_docker_image "${ZIGBEE_HOST_PUBLISH_DIRECTORY}" "haus-zigbee"
+}
