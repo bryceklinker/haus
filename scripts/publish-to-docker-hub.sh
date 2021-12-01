21b7e987dcdb33723cf6478f014090023f801367
Bryce Klinker <bryce.klinker@gmail.com>
2021-11-30 21:47:52 -0600

    Updating docker build script

--- scripts/publish-to-docker-hub.sh
+++ scripts/publish-to-docker-hub.sh
@@ -10,7 +10,10 @@ function login() {
 function build_docker_image() {
   PUBLISH_DIRECTORY="${1}"
   APPLICATION_NAME="${2}"
-  docker build . --tag "haus/${APPLICATION_NAME}:${VERSION}" --file "${APPLICATION_NAME}-dockerfile"
+  docker build --tag "haus/${APPLICATION_NAME}:${VERSION}" \
+    --file "${APPLICATION_NAME}-dockerfile" \
+    --build-arg PUBLISH_DIR="${PUBLISH_DIRECTORY}" \
+    .
 }
 
 function main() {
