a8d452e6d7bc9d25c2636d6031d42aeab4bd3d36
Bryce Klinker <bryce.klinker@gmail.com>
2021-12-09 11:34:39 -0600

    Updating to allow container to use an embedded https self-signed cert... Might work?

--- scripts/publish-to-docker-hub.sh
+++ scripts/publish-to-docker-hub.sh
@@ -3,6 +3,9 @@ set -ex
 
 source ./scripts/variables.sh
 
+export HTTPS_CERT_PATH="${WORKING_DIRECTORY}/cert.pfx"
+export HTTPS_CERT_PASSWORD=""
+
 function login() {
   docker login -u "${DOCKER_HUB_USERNAME}" -p "${DOCKER_HUB_ACCESS_TOKEN}"
 }
@@ -18,6 +21,7 @@ function build_docker_image() {
     --file "haus-dockerfile" \
     --build-arg PUBLISH_DIR="${PUBLISH_DIRECTORY}" \
     --build-arg ENTRY_FILE="${ENTRY_FILE}" \
+    --build-arg HTTPS_CERT_PASSWORD="${HTTPS_CERT_PASSWORD}" \
     .
 }
 
@@ -25,8 +29,17 @@ function publish_docker_images() {
   docker push --all-tags "${DOCKER_HUB_USERNAME}/${DOCKER_HUB_REPO}"
 }
 
+function generate_https_cert() {
+  HTTPS_CERT_PASSWORD=$(uuidgen)
+  dotnet dev-certs https --export-path "${HTTPS_CERT_PATH}" --password "${HTTPS_CERT_PASSWORD}"
+  cp "${HTTPS_CERT_PATH}" "${WEB_HOST_PUBLISH_DIRECTORY}"
+  cp "${HTTPS_CERT_PATH}" "${ZIGBEE_HOST_PUBLISH_DIRECTORY}"
+}
+
 function main() {
   login
+  
+  generate_https_cert
   
   build_docker_image "publish/haus-web" "haus-web" "Haus.Web.Host.dll"
   build_docker_image "publish/haus-zigbee" "haus-zigbee" "Haus.Zigbee.Host.dll"
