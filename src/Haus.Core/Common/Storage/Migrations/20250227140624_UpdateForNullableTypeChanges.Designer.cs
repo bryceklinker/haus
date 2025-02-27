214df23364c6366c24a36563ddc0350bd000aeaf
Bryce Klinker <bryce.klinker@upliftdelivery.com>
2025-02-27 08:07:55 -0600

    creating migration for nullable changes

--- src/Haus.Core/Common/Storage/Migrations/20250227140624_UpdateForNullableTypeChanges.Designer.cs
+++ src/Haus.Core/Common/Storage/Migrations/20250227140624_UpdateForNullableTypeChanges.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Haus.Core.Common.Storage;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Haus.Core.Common.Storage.Migrations
 {
     [DbContext(typeof(HausDbContext))]
-    partial class HausDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250227140624_UpdateForNullableTypeChanges")]
+    partial class UpdateForNullableTypeChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
