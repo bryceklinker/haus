887a5cc5dbd71d14ebbff695a978feee8cccc357
Bryce Klinker <bryce.klinker@upliftdelivery.com>
2025-05-02 08:30:06 -0500

    adjusting compose to have site served from nginx container

--- scripts/publish-to-docker-hub.sh
+++ scripts/publish-to-docker-hub.sh
@@ -7,7 +7,7 @@ function login() {
   docker login -u "${DOCKER_HUB_USERNAME}" -p "${DOCKER_HUB_ACCESS_TOKEN}"
 }
 
-function build_docker_image() {
+function build_service_docker_image() {
   PUBLISH_DIRECTORY="${1}"
   APPLICATION_NAME="${2}"
   ENTRY_FILE="${3}"
@@ -21,6 +21,18 @@ function build_docker_image() {
     .
 }
 
+function build_site_docker_image() {
+  PUBLISH_DIRECTORY="${1}"
+  APPLICATION_NAME="${2}"
+    
+  docker build \
+    --tag "${DOCKER_HUB_USERNAME}/${DOCKER_HUB_REPO}:${APPLICATION_NAME}-${VERSION}" \
+    --tag "${DOCKER_HUB_USERNAME}/${DOCKER_HUB_REPO}:${APPLICATION_NAME}-latest" \
+    --file "haus-site-dockerfile" \
+    --build-arg PUBLISH_DIR="${PUBLISH_DIRECTORY}" \
+    .
+}
+
 function publish_docker_images() {
   docker push --all-tags "${DOCKER_HUB_USERNAME}/${DOCKER_HUB_REPO}"
 }
@@ -28,10 +40,11 @@ function publish_docker_images() {
 function main() {
   login
   
-  build_docker_image "publish/haus-web" "haus-web" "Haus.Web.Host.dll"
-  build_docker_image "publish/haus-zigbee" "haus-zigbee" "Haus.Zigbee.Host.dll"
+  build_service_docker_image "publish/haus-web" "haus-web" "Haus.Web.Host.dll"
+  build_service_docker_image "publish/haus-zigbee" "haus-zigbee" "Haus.Zigbee.Host.dll"
+  build_site_docker_image "publish/haus-site" "haus-site" "Haus.Zigbee.Host.dll"
   
-  publish_docker_images
+#  publish_docker_images
 }
 
 main
