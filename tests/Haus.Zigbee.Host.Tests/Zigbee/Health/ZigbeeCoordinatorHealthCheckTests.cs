ae2f48a2ae1de4d46e43ba17ffdfb710f744c3e1
Bryce Klinker <bryce.klinker@gmail.com>
2026-07-27 22:20:18 -0500

    fix: reverting tests as health checks aren't valid for haus.zigbee.host

--- tests/Haus.Zigbee.Host.Tests/Zigbee/Health/ZigbeeCoordinatorHealthCheckTests.cs
+++ tests/Haus.Zigbee.Host.Tests/Zigbee/Health/ZigbeeCoordinatorHealthCheckTests.cs
@@ -2,7 +2,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using Haus.Zigbee.Host.Tests.Support;
 using Haus.Zigbee.Host.Zigbee.Health;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Xunit;
 
@@ -10,24 +9,13 @@ namespace Haus.Zigbee.Host.Tests.Zigbee.Health;
 
 public class ZigbeeCoordinatorHealthCheckTests
 {
-    private readonly FakeZigbeeCoordinator _coordinator;
-    private readonly ZigbeeCoordinatorHealthCheck _healthCheck;
-
-    public ZigbeeCoordinatorHealthCheckTests()
-    {
-        _coordinator = new FakeZigbeeCoordinator();
-
-        _healthCheck = ServiceProviderFactory.Create(
-            zigbeeCoordinator: _coordinator
-        ).GetRequiredService<ZigbeeCoordinatorHealthCheck>();
-    }
-    
     [Fact]
     public async Task CheckHealthAsync_CoordinatorConnected_ReturnsHealthy()
     {
-        _coordinator.IsConnected = true;
+        var coordinator = new FakeZigbeeCoordinator { IsConnected = true };
+        var healthCheck = new ZigbeeCoordinatorHealthCheck(coordinator);
 
-        var result = await _healthCheck.CheckHealthAsync(new HealthCheckContext(), CancellationToken.None);
+        var result = await healthCheck.CheckHealthAsync(new HealthCheckContext(), CancellationToken.None);
 
         Assert.Equal(HealthStatus.Healthy, result.Status);
     }
@@ -35,9 +23,10 @@ public class ZigbeeCoordinatorHealthCheckTests
     [Fact]
     public async Task CheckHealthAsync_CoordinatorNotConnected_ReturnsUnhealthy()
     {
-        _coordinator.IsConnected = false;
+        var coordinator = new FakeZigbeeCoordinator { IsConnected = false };
+        var healthCheck = new ZigbeeCoordinatorHealthCheck(coordinator);
 
-        var result = await _healthCheck.CheckHealthAsync(new HealthCheckContext(), CancellationToken.None);
+        var result = await healthCheck.CheckHealthAsync(new HealthCheckContext(), CancellationToken.None);
 
         Assert.Equal(HealthStatus.Unhealthy, result.Status);
     }
