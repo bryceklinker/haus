e0b15bfcaa6f58f33322046ed5eb1ccacaa85be1
Bryce Klinker <bryce.klinker@gmail.com>
2026-08-22 20:18:39 -0500

    feat(site-host): prepend live zigbee events to the activity feed
    Subscribes to all 6 zigbee_* event types individually since the
    SignalR client dispatches by declared type, not a dynamic wildcard.
    Part of #48.

--- tests/Haus.Site.Host.Tests/Zigbee/Activity/ZigbeeActivityViewTests.cs
+++ tests/Haus.Site.Host.Tests/Zigbee/Activity/ZigbeeActivityViewTests.cs
@@ -1,8 +1,12 @@
 using System.Linq;
 using System.Threading.Tasks;
+using Haus.Core.Models;
 using Haus.Core.Models.Common;
+using Haus.Core.Models.ExternalMessages;
 using Haus.Core.Models.Zigbee;
+using Haus.Core.Models.Zigbee.Events;
 using Haus.Site.Host.Tests.Support;
+using Haus.Site.Host.Tests.Support.Realtime;
 using Haus.Site.Host.Zigbee.Activity;
 using Haus.Testing.Support;
 using MudBlazor;
@@ -12,6 +16,12 @@ namespace Haus.Site.Host.Tests.Zigbee.Activity;
 public class ZigbeeActivityViewTests : HausSiteTestContext
 {
     private const string ActivityUrl = "/api/zigbee/activity";
+    private readonly InMemoryRealtimeDataSubscriber _eventsSubscriber;
+
+    public ZigbeeActivityViewTests()
+    {
+        _eventsSubscriber = GetSubscriber(HausRealtimeSources.Events);
+    }
 
     [Fact]
     public async Task WhenRenderedThenShowsLoadingActivity()
@@ -55,6 +65,28 @@ public class ZigbeeActivityViewTests : HausSiteTestContext
             Assert.Equal(2, entries.Length);
             Assert.Equal("newer", entries[0].Instance.Entry?.EventType);
             Assert.Equal("older", entries[1].Instance.Entry?.EventType);
+        });
+    }
+
+    [Fact]
+    public async Task WhenZigbeeEventReceivedThenPrependsToActivityFeed()
+    {
+        await HausApiHandler.SetupGetAsJson(ActivityUrl, new ListResult<ZigbeeActivityEntryModel>());
+        var view = RenderView<ZigbeeActivityView>();
+        Eventually.Assert(() =>
+        {
+            view.FindByComponent<MudText>(opts => opts.WithText("No recent zigbee activity"));
+        });
+
+        await _eventsSubscriber.SimulateAsync(
+            HausEventsEventNames.OnEvent,
+            new ZigbeeDeviceJoinedEvent("00:11:22:33:44:55:66:77", 1234).AsHausEvent()
+        );
+
+        Eventually.Assert(() =>
+        {
+            var entry = view.FindByComponent<ZigbeeActivityEntryView>();
+            Assert.Equal(ZigbeeDeviceJoinedEvent.Type, entry.Instance.Entry?.EventType);
         });
     }
 }
