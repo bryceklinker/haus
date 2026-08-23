7dd3d38e6f4f802da62273a0a605e8e3b253dba9
Bryce Klinker <bryce.klinker@gmail.com>
2026-08-22 20:10:16 -0500

    feat(site-host): refresh zigbee devices list live on join/discovery events
    Part of #48.

--- tests/Haus.Site.Host.Tests/Zigbee/Devices/ZigbeeDevicesViewTests.cs
+++ tests/Haus.Site.Host.Tests/Zigbee/Devices/ZigbeeDevicesViewTests.cs
@@ -1,8 +1,12 @@
 using System.Linq;
 using System.Threading.Tasks;
+using Haus.Core.Models;
 using Haus.Core.Models.Common;
+using Haus.Core.Models.ExternalMessages;
 using Haus.Core.Models.Zigbee;
+using Haus.Core.Models.Zigbee.Events;
 using Haus.Site.Host.Tests.Support;
+using Haus.Site.Host.Tests.Support.Realtime;
 using Haus.Site.Host.Zigbee.Devices;
 using Haus.Testing.Support;
 using MudBlazor;
@@ -12,6 +16,12 @@ namespace Haus.Site.Host.Tests.Zigbee.Devices;
 public class ZigbeeDevicesViewTests : HausSiteTestContext
 {
     private const string DevicesUrl = "/api/zigbee/devices";
+    private readonly InMemoryRealtimeDataSubscriber _eventsSubscriber;
+
+    public ZigbeeDevicesViewTests()
+    {
+        _eventsSubscriber = GetSubscriber(HausRealtimeSources.Events);
+    }
 
     [Fact]
     public async Task WhenRenderedThenShowsLoadingDevices()
@@ -57,6 +67,31 @@ public class ZigbeeDevicesViewTests : HausSiteTestContext
         Eventually.Assert(() =>
         {
             Assert.Equal(3, view.FindAllByComponent<ZigbeeDeviceView>().Count());
+        });
+    }
+
+    [Fact]
+    public async Task WhenDeviceJoinedEventReceivedThenRefreshesDevicesList()
+    {
+        await HausApiHandler.SetupGetAsJson(DevicesUrl, new ListResult<ZigbeeKnownDeviceModel>());
+        var view = RenderView<ZigbeeDevicesView>();
+        Eventually.Assert(() =>
+        {
+            view.FindByComponent<MudText>(opts => opts.WithText("No zigbee devices discovered yet"));
+        });
+
+        await HausApiHandler.SetupGetAsJson(
+            DevicesUrl,
+            new ListResult<ZigbeeKnownDeviceModel>([HausModelFactory.ZigbeeKnownDeviceModel()])
+        );
+        await _eventsSubscriber.SimulateAsync(
+            HausEventsEventNames.OnEvent,
+            new ZigbeeDeviceJoinedEvent("00:11:22:33:44:55:66:77", 1234).AsHausEvent()
+        );
+
+        Eventually.Assert(() =>
+        {
+            Assert.Single(view.FindAllByComponent<ZigbeeDeviceView>());
         });
     }
 }
